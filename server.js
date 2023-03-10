b19075ddf4449fcc9e60c60819fa6070dd1a35ab
Zehan Khan <zehan9211@gmail.com>
2023-03-10 16:28:15 +0530

    install some nesseccary deps

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3000
+require("dotenv").config();
+const express = require('express');
+const app = express();
+const logger = require('morgan');
+const port = process.env.PORT || "9000"
+
+app.use(logger("dev"))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.send('Hello from backend')
