2b6e083b30eb5f32039885153e714e7449c015e0
Zehan Khan <zehan9211@gmail.com>
2023-03-10 16:36:50 +0530

    database created, setup and connected

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 require("dotenv").config();
 const express = require('express');
-const app = express();
 const logger = require('morgan');
+const mongoose = require("mongoose");
 const port = process.env.PORT || "9000"
+const connUri = process.env.MONGO_LOCAL_CONN_URL;
+
+//=== 1 - App created
+const app = express();
+
+
+//=== 2 - SET UP DATABASE
+//Configure mongoose's promise to global promise
+mongoose.set('strictQuery', false)
+mongoose.promise = global.Promise;
+mongoose.connect(connUri);
+
+const connection = mongoose.connection;
+connection.once('open', () => console.log('MongoDB --  database connection established successfully!'));
+connection.on('error', (err) => {
+    console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
+    process.exit();
+});
 
 app.use(logger("dev"))
 app.use(express.json());
