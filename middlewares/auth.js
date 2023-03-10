e22feb199badccb92ea1968d36011561b41d8de4
Zehan Khan <zehan9211@gmail.com>
2023-03-11 00:02:10 +0530

    handle all error in middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,19 +4,23 @@ const { status, errorMessage } = require("../helpers/status");
 verifyToken = async (req, res, next) => {
     console.log(req.headers.authorization);
     let token = req.headers.authorization;
+    if (!token) {
+        errorMessage.error = 'Authentication Failed : Token not provided';
+        return res.status(status.bad).send(errorMessage);
+    }
     try {
         if (token) {
             const payload = await jwt.verify(token, process.env.SECRET);
             req.user = payload;
-            next()
+            next();
         } else {
             errorMessage.error = "Token required"
             return res.status(status.bad).json(errorMessage);
         }
     } catch (error) {
-        console.log(error.message,"error");
-        errorMessage.error = error.message
-        return res.status(status.error).json(errorMessage);
+        console.log(error.message, "error");
+        errorMessage.error = "Authentication Failed : " + error.message;
+        return res.status(status.unauthorized).json(errorMessage);
     }
 
 }
