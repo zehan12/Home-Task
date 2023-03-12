a555de5bae856484d4bd0600d467bbb15d1c1ffc
Zehan Khan <zehan9211@gmail.com>
2023-03-12 13:13:17 +0530

    add headlessUI and daisyUI for ui dev

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
+import Header from './components/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="">
+      <Header />
       <h1 className="text-3xl font-thin text-white bg-red-600 text-center underline">
         Home
       </h1>
